530fe644b4826f2be730099cdf9a83bb1c47241f
LingkonDash <cocrupon@gamil.com>
2026-03-06 22:36:25 +0600

    Added eye button in login password input "

--- scripts/login.js
+++ scripts/login.js
@@ -1,1 +1,20 @@
-console.log('Login js');
+function eyebutton() {
+  const eyeBtn = document.getElementById('eyeBtn');
+  const passInput = document.getElementById('login-password')
+
+  const eye = eyeBtn.querySelector('#eye');
+  
+  if(eye.classList.contains('fa-eye')) {
+    eye.classList.remove('fa-eye');
+    eye.classList.add('fa-eye-slash');
+
+    passInput.type = 'text'
+
+  } else {
+    eye.classList.remove('fa-eye-slash');
+    eye.classList.add('fa-eye');
+    
+    passInput.type = 'password'
+  }
+  
+}
